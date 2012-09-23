e96ecee035729040ea33b28ced7a213e5e3e0e44
Alexis Jacomy <alexis.jacomy@gmail.com>
2012-09-23 15:05:42 +0200

    added service to get user comments

--- js/osdc2012/osdc2012.reddit.js
+++ js/osdc2012/osdc2012.reddit.js
@@ -24,43 +24,44 @@
   'use strict';
 
   window.reddit = new sigma.classes.EventDispatcher();
-  var cache_pageComments = {},
+  var userAgent = 'exploring reddit with sigma.js (for a talk for OSDC2012 fr)',
+      cache_pageComments = {},
       cache_userComments = {},
       regexps = [
-    {
-      // COMMENT PERMALINK:
-      // Examples:
-      //  - http://www.reddit.com/r/programming/comments/10b7xo/bug_ios6_safari_caches_post_requests/c6c1iti
-      regex: /^https?:\/\/(?:www\.)?reddit\.com\/r\/([^\/]*)\/comments\/([^\/]*)\/([^\/]*)\/([^\/]*)\/?/,
-      method: function(match) {
-        if(!match)
-          return false;
-        else
-          return {
-            topic: match[1],
-            postid: match[2],
-            postlabel: match[3],
-            commentid: match[4],
-          };
-      }
-    }
-    {
-      // COMMENTS PERMALINK:
-      // Examples:
-      //  - http://www.reddit.com/r/programming/comments/10b7xo/bug_ios6_safari_caches_post_requests/
-      regex: /^https?:\/\/(?:www\.)?reddit\.com\/r\/([^\/]*)\/comments\/([^\/]*)\/([^\/]*)\/?$/,
-      method: function(match) {
-        if(!match)
-          return false;
-        else
-          return {
-            topic: match[1],
-            postid: match[2],
-            postlabel: match[3]
-          };
-      }
-    }
-  ];
+        {
+          // COMMENT PERMALINK:
+          // Examples:
+          //  - http://www.reddit.com/r/programming/comments/10b7xo/bug_ios6_safari_caches_post_requests/c6c1iti
+          regex: /^https?:\/\/(?:www\.)?reddit\.com\/r\/([^\/]*)\/comments\/([^\/]*)\/([^\/]*)\/([^\/]*)\/?/,
+          method: function(match) {
+            if(!match)
+              return false;
+            else
+              return {
+                topic: match[1],
+                postid: match[2],
+                postlabel: match[3],
+                commentid: match[4],
+              };
+          }
+        },
+        {
+          // COMMENTS PERMALINK:
+          // Examples:
+          //  - http://www.reddit.com/r/programming/comments/10b7xo/bug_ios6_safari_caches_post_requests/
+          regex: /^https?:\/\/(?:www\.)?reddit\.com\/r\/([^\/]*)\/comments\/([^\/]*)\/([^\/]*)\/?$/,
+          method: function(match) {
+            if(!match)
+              return false;
+            else
+              return {
+                topic: match[1],
+                postid: match[2],
+                postlabel: match[3]
+              };
+          }
+        }
+      ];
 
   reddit.parseEntity = function(entity) {
     if(!entity)
@@ -82,13 +83,13 @@
         urlObj = this.parseEntity(entity);
 
     if(!urlObj || urlObj.postid===undefined)
-      self.dispatch('pageCommentsLoaded',{
-        received: {}
+      return self.dispatch('pageCommentsLoaded',{
+        comments: {}
       });
 
     if(cache_pageComments[urlObj.postid]!==undefined)
-      self.dispatch('pageCommentsLoaded',{
-        received: cache_pageComments[urlObj.postid]
+      return self.dispatch('pageCommentsLoaded',{
+        comments: cache_pageComments[urlObj.postid]
       });
 
     (urlObj.postid!==undefined) &&
@@ -96,14 +97,56 @@
         url: 'http://www.reddit.com/r/programming/comments/'+urlObj.postid+'.json?jsonp=?',
         type: 'GET',
         dataType: 'jsonp',
+        beforeSend: function(request) {
+          request.setRequestHeader('User-Agent',userAgent);
+        },
         success: function(data){
           cache_pageComments[urlObj.postid] = data;
-          self.dispatch('pageCommentsLoaded',{
-            received: data
+          return self.dispatch('pageCommentsLoaded',{
+            comments: data
           });
         },
         error: function(jqXHR, textStatus, errorThrown){
-          self.dispatch('commentsFailed',{
+          return self.dispatch('pageCommentsFailed',{
+            jqXHR: jqXHR,
+            textStatus: textStatus,
+            errorThrown: errorThrown
+          });
+        }
+      });
+  };
+
+  reddit.userComments = function(entity, options) {
+    var o = options || {},
+        self = this,
+        urlObj = this.parseEntity(entity);
+
+    if(!urlObj || urlObj.userid===undefined)
+      return self.dispatch('userCommentsLoaded',{
+        comments: {}
+      });
+
+    if(cache_userComments[urlObj.userid]!==undefined)
+      return self.dispatch('userCommentsLoaded',{
+        comments: cache_userComments[urlObj.userid]
+      });
+
+    (urlObj.userid!==undefined) &&
+      $.ajax({
+        url: 'www.reddit.com/user/'+urlObj.userid+'/comments.json?jsonp=?',
+        type: 'GET',
+        dataType: 'jsonp',
+        beforeSend: function(request) {
+          request.setRequestHeader('User-Agent',userAgent);
+        },
+        success: function(data){
+          cache_userComments[urlObj.userid] = data;
+          return self.dispatch('userCommentsLoaded',{
+            comments: data
+          });
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+          return self.dispatch('userCommentsFailed',{
             jqXHR: jqXHR,
             textStatus: textStatus,
             errorThrown: errorThrown
